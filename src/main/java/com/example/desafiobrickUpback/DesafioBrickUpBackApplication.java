0fb9b7152dc0e59a7b3ab40ffcda2969caf394e7
RayAra2004 <raynan.araujo@gmail.com>
2023-12-17 20:30:22 -0300

    Initial commit

---
+++ src/main/java/com/example/desafiobrickUpback/DesafioBrickUpBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiobrickUpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBrickUpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBrickUpBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafiobrickUpback/DesafioBrickUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiobrickUpback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBrickUpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
