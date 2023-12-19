7a7ae741c2343a947cf5dd367116cceeaa025d56
RayAra2004 <raynan.araujo@gmail.com>
2023-12-18 21:03:54 -0300

    fix: image not required

---
+++ src/main/java/com/example/desafiobrickUpback/dtos/TaskRecordDto.java
@@ -0,0 +1,6 @@
+package com.example.desafiobrickUpback.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record TaskRecordDto(@NotBlank String description, @NotBlank String status, String image) {
+}
